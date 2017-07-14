26ee59b0660dcc508b87d28f7315c7f034db489c
igeligel <kevinigeligeligel@gmail.com>
2017-07-14 23:45:37 +0200

    refactor(components)
    refactor hello component to have simpler template, dynamic class binding
    and methods; i also add more classes to style elements

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,43 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1 :class="headingClasses">{{ msg }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'hello',
+  computed: {
+    headingClasses: function getHeadingClasses() {
+      return {
+        red: this.isCrazy,
+        blue: !this.isCrazy,
+        shadow: this.isCrazy,
+      };
+    },
+  },
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
+      isCrazy: false,
     };
+  },
+  methods: {
+    toggleClass() {
+      this.isCrazy = !this.isCrazy;
+    },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.red {
+  color: red;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
+.blue {
+  color: blue;
 }
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.shadow {
+  box-shadow: 5px 5px 5px 5px rgba(0,0,0,0.2);
 }
 </style>
